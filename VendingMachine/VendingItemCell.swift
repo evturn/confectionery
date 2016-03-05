c1e4ca0e68d574700bd1c97d177fafbfd2a2de59
Evan Turner <evturn@gmail.com>
2016-03-05 00:24:07 -0500

    Initialize repo

---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  VendingItemCell.swift
+//  Confectionery
+//
+//  Created by Evan Turner on 3/5/16.
+//  Copyright © 2016 evturn. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+    
+}
